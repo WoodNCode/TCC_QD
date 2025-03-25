bbdf5dcd51aed58840e99e4643eeff37b5ee5fde
Yannick Plüss <yannick@plyz.ch>
2025-03-25 17:22:35 +0100

    Update TCC_graphics.py

--- TCC_graphics.py
+++ TCC_graphics.py
@@ -1,4 +1,11 @@
 import drawsvg as draw
+import base64 as base64
+
+def render_svg(svg):
+    """Renders the given svg string."""
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
 
 def draw_elevation_view(L, s, show_legend=False):
     """
