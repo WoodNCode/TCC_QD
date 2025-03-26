cd2d9ce0cace0ccc858a3fa2cace08af6dd472eb
Yannick Plüss <yannick@plyz.ch>
2025-03-26 10:55:21 +0100

    Update TCC_graphics.py

--- TCC_graphics.py
+++ TCC_graphics.py
@@ -1,5 +1,12 @@
 import drawsvg as draw
 import math
+import streamlit as st
+
+def render_svg(svg):
+    """Renders the given svg string."""
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
 
 def draw_elevation_view_scaled(L, s):
     """
